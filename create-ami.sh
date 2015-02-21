c4e28b2b4f21b407dce2dc68c008eb727a2ab7b6
Umesh Mishra <webmakerz92@gmail.com>
2015-02-21 21:09:08 +0530

    Building a amazon custom AMI

--- create-ami.sh
+++ create-ami.sh
@@ -3,7 +3,7 @@
 
 # This script will build a Custom Amazon EC2 Machine Image 
 
-export EC2_HOME=/opt/ec2/tools                           # Make sure you use the AMI tools you can download using curl -o /tmp/ec2-ami-tools.zip http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip
+export EC2_HOME=/opt/ec2/tools                           # Your EC2 tools folder
 export EC2_URL=https://ec2.amazonaws.com
 export AWS_ACCOUNT_NUMBER='aws-account-number'           # Your Amazon S3 User Id
 export AWS_ACCESS_KEY_ID='your_access_key_id'            # Your Amazon S3 Access Key
@@ -18,15 +18,14 @@ export JAVA_HOME=/usr/local/java/jre1.7.0_75              # have to edit ec2-cmd
 
 
 
-# This variable is for deleting the files from the /mnt/ directory after bundling
-shortname=ami-image
-#Create the server image filename and manifest filename with todays date ami-image.21.02.2015 and ami-image.21.02.2015.manifest.xml
-filename=ami-image
 
+#Create the server image filename and manifest filename with todays date filename.21.02.2015 and filename.21.02.2015.manifest.xml
+filename=ami-image		#change filename to your necessity
 
 
 
 excludes=/proc
+
 #bundle the server, upload it, export necessary variables and register it,
 ec2-bundle-vol -d /mnt -k $EC2_PRIVATE_KEY -c $EC2_CERT -u $AWS_ACCOUNT_NUMBER -r x86_64 -p $filename.$(date +%d.%m.%Y) 
 ec2-upload-bundle -b $AWS_AMI_BUCKET -m /mnt/ami-$filename.$(date +%d.%m.%Y).manifest.xml -a $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY 
@@ -35,5 +34,7 @@ ec2-register -n $AWS_AMI_BUCKET -prd -K $EC2_PRIVATE_KEY -C $EC2_CERT $AWS_AMI_B
 
 
 
+# This variable is for deleting the files from the /mnt/ directory after bundling make sure you set the below var with intial image name which is common through all the files 
+commonName=name
 #Deleting the bundle files from the /mnt directory
-rm -Rf /mnt/$shortname
+rm -Rf /mnt/$commonName*
